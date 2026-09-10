275527d767785a19308dfe83cddfd3856c878450
Cauê Silva Dias <caue.silva.dias23@gmail.com>
2026-09-10 09:30:28 -0300

    feat: CustomUserDetailsService

---
+++ Markdown-Notes/src/main/java/com/example/Markdown_Notes/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.Markdown_Notes.infra.security;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.example.Markdown_Notes.user.User;
+import com.example.Markdown_Notes.user.UserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService{
+    @Autowired 
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
