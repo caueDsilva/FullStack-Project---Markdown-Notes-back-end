0a68f558db0cd6bb3ad1fe90b3d3baf8845cc754
Cauê Silva Dias <caue.silva.dias23@gmail.com>
2026-09-18 16:21:45 -0300

    feat : AuthenticationManager from SecurityConfig,java

--- Markdown-Notes/src/main/java/com/example/Markdown_Notes/infra/security/SecurityConfig.java
+++ Markdown-Notes/src/main/java/com/example/Markdown_Notes/infra/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,4 +37,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
     
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
